78233e05896882b0ba1bd46b2f0b34914f011dad
kedar-devs <67545340+kedar-devs@users.noreply.github.com>
2020-10-07 00:17:01 -0700

    added attack mode for j

--- Player/player.gd
+++ Player/player.gd
@@ -1,4 +1,10 @@
 extends KinematicBody2D
+enum{
+	MOVE,
+	ROLL,
+	ATTACK
+}
+var state=MOVE
 var velocity=Vector2.ZERO
 const friction=400
 const Acceleration=250
@@ -7,11 +13,20 @@ const MAX_SPEED=80
 onready var animationPlayer=$AnimationPlayer
 onready var animationTree=$AnimationTree
 onready var animationState=animationTree.get("parameters/playback")
-#func _ready():
-#	animationPlayer=$AnimationPlayer
-	
-	 # Replace with function body.
+func _ready():
+	animationTree.active=true
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+		ROLL:
+			pass
+	
+	
+func move_state(delta):
 	var input_vector=Vector2.ZERO
 	input_vector.x=Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y=Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -21,6 +36,8 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/idle/blend_position",input_vector)
 		animationTree.set("parameters/run/blend_position",input_vector)
+		animationTree.set("parameters/Attack/blend_position",input_vector)
+		
 		animationState.travel("run")
 		
 		velocity=velocity.move_toward(input_vector*MAX_SPEED,Acceleration*delta)
@@ -31,8 +48,15 @@ func _physics_process(delta):
 		velocity=velocity.move_toward(Vector2.ZERO,friction*delta)
 		
 	
-	move_and_slide(velocity)
+	velocity=move_and_slide(velocity)
+	if Input.is_action_just_pressed("attack"):
+		state=ATTACK
+
+func attack_state(delta):
+	velocity=Vector2.ZERO
+	animationState.travel("Attack")
+func attack_animation_finished():
+	state=MOVE
 	
 	
-
-
+	
